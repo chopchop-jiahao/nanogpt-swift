19837438ad352fccd880f97eb958740965c6f6d7
Jiahao <chopchop.jiahao@gmail.com>
2026-06-28 22:13:37 +0100

    load data from input.txt

--- Sources/nanogpt/nanogpt.swift
+++ Sources/nanogpt/nanogpt.swift
@@ -1,9 +1,22 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
+import Foundation
+
 @main
 struct nanogpt {
     static func main() {
-        print("Hello, world!")
+        let text = loadData()
+        
+        print(text)
+    }
+    
+    private static func loadData() -> String {
+        guard let url = Bundle.module.url(forResource: "input", withExtension: "txt"),
+              let data = try? String(contentsOf: url, encoding: .utf8) else {
+            return "Fail to load data"
+        }
+        
+        return data
     }
 }

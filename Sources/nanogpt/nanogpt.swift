fddc363085a86be832c601d54623fe0cd76c25ce
Jiahao <chopchop.jiahao@gmail.com>
2026-06-28 22:48:54 +0100

    create mapping between characters and integers

--- Sources/nanogpt/nanogpt.swift
+++ Sources/nanogpt/nanogpt.swift
@@ -3,6 +3,7 @@
 
 import Foundation
 
+@available(macOS 10.15, *)
 @main
 struct nanogpt {
     static func main() {
@@ -16,6 +17,11 @@ struct nanogpt {
         let vocabSize = chars.count
         print("chars: \(chars)")
         print("vocab size: \(vocabSize)")
+        
+        let (stoi, itos) = buildDictionaries(from: chars)
+        
+        print(encode("hii there", using: stoi))
+        print(decode(encode("hii there", using: stoi), using: itos))
     }
     
     private static func loadData() -> String {
@@ -25,5 +31,25 @@ struct nanogpt {
         }
         
         return data
+    }
+    
+    private static func buildDictionaries(from chars: [Character]) -> (stoi: [Character : Int], itos: [Int : Character]){
+        var stoi = [Character : Int]()
+        var itos = [Int : Character]()
+        for (i, char) in chars.enumerated() {
+            stoi[char] = i
+            itos[i] = char
+        }
+        
+        return (stoi, itos)
+    }
+    
+    private static func encode(_ text: String, using dictionary: [Character : Int]) -> [Int] {
+        return text.compactMap { dictionary[$0] }
+    }
+    
+    private static func decode(_ code: [Int], using dictionary: [Int : Character]) -> String {
+        let charArray = code.compactMap { dictionary[$0] }
+        return String(charArray)
     }
 }
